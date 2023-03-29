a785a951bca1bc47fe361d19847d4446376059de
ganiyuolalekan <ganiyuolalekanmatthew@gmail.com>
2023-03-29 12:14:48 +0100

    Batch Updates

--- insight_highlights/api_call.py
+++ insight_highlights/api_call.py
@@ -1,6 +1,6 @@
 import requests
-from transformers import pipeline
-from happytransformer import HappyTextToText, TTSettings
+# from transformers import pipeline
+# from happytransformer import HappyTextToText, TTSettings
 
 
 def file_extractor(file_url):
@@ -13,20 +13,20 @@ def file_extractor(file_url):
     ).text)['body']
 
 
-def clean_text(doc):
-    """Hugging Face API for grammar correction"""
-
-    return HappyTextToText(
-        "T5", "vennify/t5-base-grammar-correction"
-    ).generate_text(
-        doc, args=TTSettings(num_beams=5, min_length=1)
-    ).text
-
-
-def correct_grammar(doc):
-    """"Hugging Face API for grammar correction"""
-
-    return pipeline(
-        'text2text-generation',
-        'pszemraj/flan-t5-large-grammar-synthesis',
-    )(doc)
+# def clean_text(doc):
+#     """Hugging Face API for grammar correction"""
+#
+#     return HappyTextToText(
+#         "T5", "vennify/t5-base-grammar-correction"
+#     ).generate_text(
+#         doc, args=TTSettings(num_beams=5, min_length=1)
+#     ).text
+#
+#
+# def correct_grammar(doc):
+#     """"Hugging Face API for grammar correction"""
+#
+#     return pipeline(
+#         'text2text-generation',
+#         'pszemraj/flan-t5-large-grammar-synthesis',
+#     )(doc)
